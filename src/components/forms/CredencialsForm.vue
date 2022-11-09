05a6a0fa2468c44064cb219876caa2a4c8f27017
AdriCQ <adrian.capote95@zoho.com>
2022-11-09 15:51:17 -0500

    Auth user

--- src/components/forms/CredencialsForm.vue
+++ src/components/forms/CredencialsForm.vue
@@ -1,111 +1,92 @@
 <template>
-  <v-form v-model="valid" lazy-validation>
-    <v-row>
-      <v-col cols="12">
-        <!-- UserName -->
-        <v-text-field
-          v-model="userName"
-          :rules="userNameRules"
-          label="Usuario"
-          dense
-          required
-          outlined
-        />
-      </v-col>
+    <v-form v-model="valid" lazy-validation>
+        <v-row>
+            <v-col cols="12">
+                <!-- UserName -->
+                <v-text-field v-model="userName" :rules="userNameRules" label="Usuario" dense required outlined />
+            </v-col>
 
-      <v-col cols="12">
-        <!-- Password -->
-        <v-text-field
-          v-model="password"
-          :counter="20"
-          :rules="passwordRules"
-          label="Contraseña"
-          dense
-          required
-          outlined
-          :append-icon="show.password ? 'mdi-eye' : 'mdi-eye-off'"
-          :type="show.password ? 'text' : 'password'"
-          @click:append="show.password = !show.password"
-        />
-      </v-col>
+            <v-col cols="12">
+                <!-- Password -->
+                <v-text-field v-model="password" :counter="20" :rules="passwordRules" label="Contraseña" dense required
+                    outlined :append-icon="show.password ? 'mdi-eye' : 'mdi-eye-off'"
+                    :type="show.password ? 'text' : 'password'" @click:append="show.password = !show.password" />
+            </v-col>
 
-      <v-col cols="12">
-        <!-- Last Name -->
-        <v-text-field
-          v-model="repeatPassword"
-          :counter="20"
-          :rules="[...repeatPasswordRules, matchingPasswords]"
-          label="Confirmar Contraseña"
-          dense
-          required
-          outlined
-          :append-icon="show.repeatPassword ? 'mdi-eye' : 'mdi-eye-off'"
-          :type="show.repeatPassword ? 'text' : 'password'"
-          @click:append="show.repeatPassword = !show.repeatPassword"
-        />
-      </v-col>
-    </v-row>
+            <v-col cols="12">
+                <!-- Last Name -->
+                <v-text-field v-model="repeatPassword" :counter="20"
+                    :rules="[...repeatPasswordRules, matchingPasswords]" label="Confirmar Contraseña" dense required
+                    outlined :append-icon="show.repeatPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                    :type="show.repeatPassword ? 'text' : 'password'"
+                    @click:append="show.repeatPassword = !show.repeatPassword" />
+            </v-col>
+        </v-row>
 
-    <v-row>
-      <v-col cols="12">
-        <v-btn
-          color="primary"
-          class="font-weight-bold text-body-1"
-          @click="createAccount"
-        >
-          Crear Cuenta
-        </v-btn>
-      </v-col>
-    </v-row>
-  </v-form>
+        <v-row>
+            <v-col cols="12">
+                <v-btn color="primary" class="font-weight-bold text-body-1" @click="createAccount">
+                    Crear Cuenta
+                </v-btn>
+            </v-col>
+        </v-row>
+    </v-form>
 </template>
 
 <script>
+import { HandleAxios } from '@/services/handler';
+import { AuthService } from '@/services/auth.service';
+import { userStore } from '@/store/users.store';
+
 export default {
-  name: "CredencialsForm",
-  data: () => ({
-    // Form data
-    valid: false,
-    password: "",
-    repeatPassword: "",
-    show: {
-      password: false,
-      repeatPassword: false,
-    },
-    passwordRules: [
-      (v) => !!v || "Contraseña requerida",
-      (v) =>
-        (v && v.length <= 20) ||
-        "La contraseña no debe tener más de 20 caracteres",
-    ],
-    repeatPasswordRules: [
-      (v) => !!v || "Contraseña requerida",
-      (v) =>
-        (v && v.length <= 20) ||
-        "La contraseña no debe tener más de 20 caracteres",
-    ],
-    userName: "",
-    userNameRules: [(v) => !!v || "Usuario requerido"],
-  }),
+    name: "CredencialsForm",
+    data: () => ({
+        // Form data
+        valid: false,
+        password: "",
+        repeatPassword: "",
+        show: {
+            password: false,
+            repeatPassword: false,
+        },
+        passwordRules: [
+            (v) => !!v || "Contraseña requerida",
+            (v) =>
+                (v && v.length <= 20) ||
+                "La contraseña no debe tener más de 20 caracteres",
+        ],
+        repeatPasswordRules: [
+            (v) => !!v || "Contraseña requerida",
+            (v) =>
+                (v && v.length <= 20) ||
+                "La contraseña no debe tener más de 20 caracteres",
+        ],
+        userName: "",
+        userNameRules: [(v) => !!v || "Usuario requerido"],
+    }),
 
-  methods: {
-    /**
-     *
-     */
-    matchingPasswords() {
-      if (this.password === this.repeatPassword) {
-        return true;
-      } else {
-        return "Ambas contraseñas deben ser iguales";
-      }
-    },
+    methods: {
+        /**
+         *
+         */
+        matchingPasswords() {
+            if (this.password === this.repeatPassword) {
+                return true;
+            } else {
+                return "Ambas contraseñas deben ser iguales";
+            }
+        },
 
-    /**
-     * Create Account
-     */
-    createAccount() {
-      console.log("Create Account");
+        /**
+         * Create Account
+         */
+        createAccount() {
+            HandleAxios(AuthService().login(this.userName)).then(r => {
+                userStore().setProfile(r);
+            }).finally(() => {
+                this.$router.push('/')
+            })
+        },
     },
-  },
 };
 </script>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,85 +1,70 @@
- <template>
-  <div id="login-view">
-    <v-card max-width="430" elevation="10" class="rounded-lg px-2 py-4 mx-auto">
-      <v-card-title class="text-h6 font-weight-medium">
-        Inicia Sesión
-      </v-card-title>
+<template>
+    <div id="login-view">
+        <v-card max-width="430" elevation="10" class="rounded-lg px-2 py-4 mx-auto">
+            <v-card-title class="text-h6 font-weight-medium">
+                Inicia Sesión
+            </v-card-title>
 
-      <v-card-text>
-        <v-form ref="form" v-model="valid" lazy-validation>
-          <v-text-field
-            v-model="username"
-            :rules="usernameRules"
-            label="Usuario"
-            required
-            outlined
-          />
+            <v-card-text>
+                <v-form ref="form" v-model="valid" lazy-validation>
+                    <v-text-field v-model="username" :rules="usernameRules" label="Usuario" required outlined />
 
-          <v-text-field
-            v-model="password"
-            :counter="20"
-            :rules="passwordRules"
-            label="Contraseña"
-            required
-            outlined
-            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-            :type="showPassword ? 'text' : 'password'"
-            @click:append="showPassword = !showPassword"
-            @keydown.enter="submit"
-          />
+                    <v-text-field v-model="password" :counter="20" :rules="passwordRules" label="Contraseña" required
+                        outlined :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                        :type="showPassword ? 'text' : 'password'" @click:append="showPassword = !showPassword"
+                        @keydown.enter="submit" />
 
-          <v-btn
-            color="primary"
-            block
-            submit
-            dark
-            class="mx-auto font-weight-bold text-body-1 mb-5"
-            @click="login"
-          >
-            Iniciar
-          </v-btn>
+                    <v-btn color="primary" block submit dark class="mx-auto font-weight-bold text-body-1 mb-5"
+                        @click="login">
+                        Iniciar
+                    </v-btn>
 
-          <div class="text-body-1">
-            ¿No tiene una cuenta en True Vision aún?
-          </div>
-          <v-btn
-            color="primary"
-            class="font-weight-bold text-body-1"
-            text
-            :to="{ name: ROUTES.REGISTER }"
-          >
-            Crear Cuenta
-          </v-btn>
-        </v-form>
-      </v-card-text>
-    </v-card>
-  </div>
+                    <div class="text-body-1">
+                        ¿No tiene una cuenta en True Vision aún?
+                    </div>
+                    <v-btn color="primary" class="font-weight-bold text-body-1" text :to="{ name: ROUTES.REGISTER }">
+                        Crear Cuenta
+                    </v-btn>
+                </v-form>
+            </v-card-text>
+        </v-card>
+    </div>
 </template>
 
 <script>
+import { AuthService } from '@/services/auth.service';
+import { HandleAxios } from '@/services/handler';
+import { userStore } from '@/store/users.store';
 import { ROUTES } from "/src/router/names";
 
 export default {
-  name: "LoginView",
-  data: () => ({
-    valid: true,
-    password: "",
-    showPassword: false,
-    passwordRules: [
-      (v) => !!v || "Contraseña requerida",
-      (v) => (v && v.length <= 20) || "La contraseña excede los 20 caracteres",
-    ],
-    username: "",
-    usernameRules: [(v) => !!v || "Usuario requerido"],
-    checkbox: false,
-    ROUTES: ROUTES,
-  }),
+    name: "LoginView",
+    data: () => ({
+        valid: true,
+        password: "",
+        showPassword: false,
+        passwordRules: [
+            (v) => !!v || "Contraseña requerida",
+            (v) => (v && v.length <= 20) || "La contraseña excede los 20 caracteres",
+        ],
+        username: "",
+        usernameRules: [(v) => !!v || "Usuario requerido"],
+        checkbox: false,
+        ROUTES: ROUTES,
+    }),
 
-  methods: {
-    login() {
-      console.log("Login");
-      ///TODO: Login request
+    methods: {
+        login() {
+            HandleAxios(AuthService().login(this.userName)).then(r => {
+                userStore().setProfile(r);
+                // userStore().setProfile({
+                //     usuario: "Adrian",
+                //     password: "Adrian",
+                //     rol: 'E'
+                // });
+                this.$router.push('/')
+            });
+        },
     },
-  },
 };
 </script>
